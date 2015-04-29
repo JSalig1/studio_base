9c3e98d400be503aa167d20ae79273abe602a5db
jsalig1 <jeremy.salig@gmail.com>
2015-04-29 12:52:22 -0400

    Adds unit test for status

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe Project, type: :model do
   subject { create(:project) }
 
+  it { should define_enum_for(:status)
+           .with([:in_progress, :completed])}
+
   describe 'validations' do
     it { should validate_presence_of(:job_number) }
     it { should validate_uniqueness_of(:job_number) }
