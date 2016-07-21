fff861b4a597f9bbb5319a69fa22a65de40c53cf
jsalig1 <jeremy.salig@gmail.com>
2016-07-21 11:00:48 -0400

    Clean up factories
    There were factories in there that we no longer use. Slim and trim!

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,27 +1,8 @@
 FactoryGirl.define do
-  sequence :job_number do |n|
-    "project_number#{n}"
-  end
-
-  sequence :name do |n|
-    "drive_number#{n}"
-  end
-
-  factory :project do
-    name 'project_name'
-    job_number
-  end
 
   factory :drive do
     name "name"
     project "project"
   end
 
-  factory :archive do
-    project_id 1
-    drive_id 1
-
-    project
-    drive
-  end
 end
